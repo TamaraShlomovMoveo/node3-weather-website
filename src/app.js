ad564a840e520d40c7c5c3bba9e20cdf46b8c98a
TamaraShlomovMoveo <tamara@moveo.co.il>
2019-04-15 17:51:28 +0300

    hayy

--- src/app.js
+++ src/app.js
@@ -1,43 +1,102 @@
-const path=require('path')
-const express=require('express')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-console.log(__dirname)
-//console.log(__filename)
-console.log(path.join(__dirname,'../public'))
+const app = express()
+const port = process.env.PORT || 3000
 
-const app=express()
-app.set('view engine','hbs')
-app.use(express.static(path.join(__dirname,'../public')))
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-// app.get('',(req,res)=>{ //localhost:3000
-//     res.send('Hellow express!')
-// }) // params: rout,function that we describe what we wanna do when someone visit this spesipic roiut
- 
-app.get( '/help',(req,res) => { //localhost:3000/help
-    // res.send({
-    // name: 'tom',
-    // age:22 })
-    res.render('help',{
-        title:'helpxxxx'
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
     })
 })
 
-// app.get('/about',(req,res)=>{ //localhost:3000/about
-//     res.send('<h1>About</h1>')
-// }) 
- app.get('/weather',(req,res)=>{ //localhost:3000/weather
-    res.send(
-        {
-            forcast:'it is sunny day',
-            location:'tel aviv israel'
-        }  )
- })
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
 
- //app.com root rout
-//app.com/help
-//app.com/about
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
+})
 
-app.listen(3000,() => {
-    console.log('Server is up  on port 3000')
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
